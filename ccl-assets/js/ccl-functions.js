ada61a0284e0862317274873710ff377e3916167
Codex42 <martin@codex42.be>
2022-09-29 23:16:27 +0000

    css

--- ccl-assets/js/ccl-functions.js
+++ ccl-assets/js/ccl-functions.js
@@ -202,9 +202,8 @@
          breakpoint: 1024,
          settings: {
            slidesToShow: 3,
-           slidesToScroll: 3,
-           infinite: true,
-           dots: true
+           slidesToScroll: 3
+
          }
        },
        {
